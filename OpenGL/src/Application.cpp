6140a011db5afd42bcef34820e9238cd0d351de7
Michael Phelps <mpphelps@gmail.com>
2022-07-28 15:04:36 -0400

    Added GLEW dependency

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -7,6 +8,8 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glewInit();
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -24,6 +27,12 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
