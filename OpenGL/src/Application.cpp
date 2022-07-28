91d37364b18f75ea97ac23938a0d9762292104f6
Michael Phelps <mpphelps@gmail.com>
2022-07-28 17:54:18 -0400

    Added statement glewInit and GL_Version

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -9,7 +10,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    glewInit();
+ 
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -21,6 +22,11 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "error" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
